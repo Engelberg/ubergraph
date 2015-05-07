f5589c1286c23672c5c0dd60247ac02cbd97d29f
Mark Engelberg <mark.engelberg@gmail.com>
2015-05-06 19:53:45 -0700

    Fixed source links

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
                  [dorothy "0.0.6"]
                  [potemkin "0.3.13"]]
   :codox {:include [ubergraph.core ubergraph.alg]
-          :src-dir-uri "http://github.com/Engelberg/ubergraph/tree/master/src/"
+          :src-dir-uri "http://github.com/Engelberg/ubergraph/tree/master/"
           :src-linenum-anchor-prefix "L"}
   )
