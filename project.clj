ce1dd44a351b5e18537272cb9334d9f00e8f5c80
Mark Engelberg <mark.engelberg@gmail.com>
2015-07-08 19:13:02 -0700

    Updated project for 0.1.4

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ubergraph "0.1.3"
+(defproject ubergraph "0.1.4"
   :description "Feature-loaded graph implementation"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.priority-map "0.0.5"]
                  [aysylu/loom "0.5.0"]
                  [dorothy "0.0.6"]
